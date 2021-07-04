eaed22f6ed2ea066fc21573f8f04dc99f103d09d
Venkataramana <34304071+Venkataramana2017@users.noreply.github.com>
2021-07-04 21:27:11 +0100

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,67 @@
+resource "aws_vpc" "Test_VPCc" {
+  cidr_block       = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Test_VPC"
+  }
+}
+
+resource "aws_subnet" "private_us_east_1a" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Public Subnet us-east-1a"
+  }
+}
+
+resource "aws_subnet" "private_us_east_1b" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Private Subnet us-east-1b"
+  }
+}
+
+resource "aws_subnet" "private_us_east_1c" {
+  vpc_id     = aws_vpc.Test_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Private Subnet us-east-1b"
+  }
+}
+resource "aws_internet_gateway" "test_vpc_igw" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  tags = {
+    Name = "Test_VPC - Internet Gateway"
+  }
+}
+
+resource "aws_route_table" "my_vpc_public" {
+    vpc_id = aws_vpc.test_vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.test_vpc_igw.id
+    }
+
+    tags = {
+        Name = "Private Subnets Route Table for Test_VPC"
+    }
+}
+
+resource "aws_route_table_association" "Test_vpc_us_east_1a_private" {
+    subnet_id = aws_subnet.private_us_east_1a.id
+    route_table_id = aws_route_table.Test_vpc_private.id
+}
+
+resource "aws_route_table_association" "Test_vpc_us_east_1b_private" {
+    subnet_id = aws_subnet.private_us_east_1b.id
+    route_table_id = aws_route_table.Test_vpc_private.id
